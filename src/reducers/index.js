e639d6d8941067df6911f2d5fcaf05a17fb665e6
Brienyll <Brienyll29@gmail.com>
2019-09-09 23:21:24 -0700

    added reducers and enabled prettier

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+  return [
+    { title: 'Macarena', duration: '4:21' },
+    { title: 'All Star', duration: '3:25' },
+    { title: 'Bitter Sweet Symphony', duration: '4:29' },
+    { title: 'Semi-Charmed Life', duration: '2:48' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+};
